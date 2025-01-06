af547c754994c1747659b0011e013b55be7b0bbc
Nitish Kumar <nitishkqr841@gmail.com>
2025-01-06 23:29:32 +0530

    Add Data Annotation to Category Model

--- Practice/Models/Category.cs
+++ Practice/Models/Category.cs
@@ -5,6 +5,8 @@
 // Created: January 6, 2025
 // ----------------------------------------------------------------------
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Practice.Models
 {
     /// <summary>
@@ -13,13 +15,15 @@ namespace Practice.Models
     public class Category
     {
         /// <summary>
-        /// Gets or sets the unique identifier for the category
+        /// Gets or sets the unique identifier for the category or Primary key
         /// </summary>
+        [Key]
         public int Id { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the category
         /// </summary>
+        [Required]
         public string Name { get; set; }
 
         /// <summary>
