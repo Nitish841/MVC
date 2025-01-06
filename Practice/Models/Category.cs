deb05584e43b1bebd704b0743bfc4f9a0504d515
Nitish Kumar <nitishkqr841@gmail.com>
2025-01-06 22:55:21 +0530

    Create Category Model

---
+++ Practice/Models/Category.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------
+// File: Category.cs
+// Description: Define the category class for managing category
+// Author: Nitish Kumar
+// Created: January 6, 2025
+// ----------------------------------------------------------------------
+
+namespace Practice.Models
+{
+    /// <summary>
+    /// Represent a category with identifier, name and display order
+    /// </summary>
+    public class Category
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the category
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the category
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display order of the category
+        /// </summary>
+        public int DisplayOrder { get; set; }
+    }
+}
