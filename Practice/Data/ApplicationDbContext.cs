6fc055a4400a794aa80fa49f013b7d337b67962d
Nitish Kumar <nitishkqr841@gmail.com>
2025-01-07 22:58:52 +0530

    Setup ApplicationDbContext, Create MVC database and Category table

---
+++ Practice/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practice.Models;
+
+namespace Practice.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        DbSet<Category> Categories { get; set; }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Practice.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+// Add Dbcontext to services
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
