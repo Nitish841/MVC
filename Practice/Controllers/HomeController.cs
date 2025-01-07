190d29a2d53e6076eeb0bc1ec0796a6799d5f759
Nitish Kumar <nitishkqr841@gmail.com>
2025-01-07 23:54:36 +0530

    Add Category Controller and Seed Category Table

--- Practice/Controllers/HomeController.cs
+++ Practice/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace Practice.Controllers
         {
             return View();
         }
-        
-        public IActionResult Category()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Practice/Migrations/20250107181931_SeedData.Designer.cs
+++ Practice/Migrations/20250107181931_SeedData.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Practice.Data;
 
@@ -10,9 +11,11 @@ using Practice.Data;
 namespace Practice.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250107181931_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
