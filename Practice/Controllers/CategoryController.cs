c22d491359fe46b7cee6998569fbd9f8526dd9ec
Nitish Kumar <nitishkqr841@gmail.com>
2025-01-16 23:02:53 +0530

    Update and Delete Category

--- Practice/Controllers/CategoryController.cs
+++ Practice/Controllers/CategoryController.cs
@@ -37,5 +37,58 @@ namespace Practice.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryObj = _db.Categories.Find(id);
+            return View(categoryObj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if(category.Name == (category.DisplayOrder).ToString())
+            {
+                ModelState.AddModelError("Name", "Category name and display order can not be same");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj != null)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
